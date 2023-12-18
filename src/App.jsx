05fc663d3cdb61fe2c5e0a713b12e9c5badcf09f
Aswanthachu <pnkaswanthachu546@gmail.com>
2023-12-18 08:42:18 +0530

    connected with new api and hardcoded datas are removed.

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,8 @@ import { useEffect, useState } from "react";
 import { CategoryTab, Header, MenuItem, SkeltonLoading } from "./components";
 import axios from "axios";
 
-import { data } from "./lib/data";
-
 function App() {
-  // const [data, setData] = useState(null);
+  const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [selectedCategory, setSelectedCategory] = useState(null);
@@ -15,17 +13,7 @@ function App() {
     const fetchData = async () => {
       try {
         const response = await axios.get(import.meta.env.VITE_API_URL);
-        // setData(response.data);
-        // let addSquareBracket = JSON.stringify("[\n" + response.data).replace(
-        //   /([{,]\s*)([a-zA-Z0-9_]+)\s*:/g,
-        //   '$1"$2":'
-        // );
-        // let updated=JSON.stringify(addSquareBracket);
-        // console.log(JSON.parse(addSquareBracket));
-        // let J=JSON.parse(updated)
-        // console.log(typeof J);
-        // console.log(J?.[0]);
-        // setData(JSON.parse(JSON.parse(addSquareBracket)));
+        setData(response.data.data);
       } catch (error) {
         setError(error.message || "An error occurred while fetching data.");
       } finally {
@@ -52,11 +40,11 @@ function App() {
 
   return (
     <main className=" p-0 lg:p-5 font-main">
-      <Header data={data[0]} cartCount={cartCount} />
+      <Header data={data?.[0]} cartCount={cartCount} />
       <div className=" lg:mt-4 w-full flex overflow-hidden hover:overflow-x-auto styled-scrollbar lg:pt-2">
-        {data[0].table_menu_list.map((menu) => (
+        {data?.[0]?.table_menu_list.map((menu) => (
           <CategoryTab
-            key={menu.menu_category_id}
+            key={menu?.menu_category_id}
             menu={menu}
             selectedCategory={selectedCategory}
             setSelectedCategory={setSelectedCategory}
@@ -64,9 +52,9 @@ function App() {
         ))}
       </div>
       <div className="w-full">
-        {selectedCategory.category_dishes.map((dish) => (
+        {selectedCategory?.category_dishes.map((dish) => (
           <MenuItem
-            key={dish.dish_id}
+            key={dish?.dish_id}
             setCartCount={setCartCount}
             dish={dish}
           />
--- src/components/CategoryTab.jsx
+++ src/components/CategoryTab.jsx
@@ -1,10 +1,10 @@
 import { cn } from "../lib/utils";
 
-const CategoryTab = ({ menu, selectedCategory, setSelectedCategory }) => {
+const CategoryTab = ({ menu, selectedCategory,setSelectedCategory}) => {
   return (
     <div
       className={cn(
-        menu.menu_category_id === selectedCategory.menu_category_id
+        menu?.menu_category_id === selectedCategory?.menu_category_id
           ? "border-red-500"
           : "border-gray-300/90",
         "min-w-[170px] md:min-w-[250px] lg:min-w-[400px] w-full h-10 flex items-center text-center justify-center  border-b-[4px] mb-[2px]"
@@ -12,12 +12,12 @@ const CategoryTab = ({ menu, selectedCategory, setSelectedCategory }) => {
     >
       <p
         className={cn(
-          menu.menu_category_id === selectedCategory.menu_category_id && "text-red-500",
+          menu?.menu_category_id === selectedCategory?.menu_category_id && "text-red-500",
           "text-sm md:text-lg font-semibold hover:cursor-pointer"
         )}
         onClick={() => setSelectedCategory(menu)}
       >
-        {menu.menu_category}
+        {menu?.menu_category}
       </p>
     </div>
   );
